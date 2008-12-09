891f13da5ba61734490efba1096fbc31736938c0
Neil Crosby <neil@thetenwordreview.com>
2008-12-09 08:09:41 +0000

    Converted tabs to spaces and fixed indentation

--- refresh.php
+++ refresh.php
@@ -1,7 +1,7 @@
 <?php
 
 function __autoload($class_name) {
-  require_once $class_name . '.php';
+    require_once $class_name . '.php';
 }
 
 $considered = getConsideredThoughts();
@@ -9,7 +9,7 @@ $stream = getStreamOfConsciousness();
 $others = getOthersSaid();
 
 if ( !$considered || !$stream || !$others ) {
-  return;
+    return;
 }
 
 $template = getTemplate();
@@ -28,11 +28,11 @@ header("Location: http://$server/");
 
 
 function getTemplate() {
-  return file_get_contents($_SERVER['DOCUMENT_ROOT'].'/templates/index.tpl');
+    return file_get_contents($_SERVER['DOCUMENT_ROOT'].'/templates/index.tpl');
 }
 
 function saveOutput( $template ) {
-  return file_put_contents($_SERVER['DOCUMENT_ROOT'].'/index.html', $template);
+    return file_put_contents($_SERVER['DOCUMENT_ROOT'].'/index.html', $template);
 }
 
 
@@ -57,304 +57,304 @@ function getOthersSaid() {
 }
 
 function getHtmlForStream($stream, $url) {
-	$data = getDataFromFeed( $url );
+    $data = getDataFromFeed( $url );
 
-	$backlog = array();
-	$output = "<ol>";
-	foreach ( $data->channel->item as $item ) {
-		$output .= getHtmlForEntry($item, $stream, &$backlog);
-	}
-	
-	if ( sizeof($backlog) > 0) {
-		$output .= getHtmlForEntry($item, $stream, &$backlog);
-	}
-	
-	$output .= "</ol>";
+    $backlog = array();
+    $output = "<ol>";
+    foreach ( $data->channel->item as $item ) {
+        $output .= getHtmlForEntry($item, $stream, &$backlog);
+    }
+    
+    if ( sizeof($backlog) > 0) {
+        $output .= getHtmlForEntry($item, $stream, &$backlog);
+    }
+    
+    $output .= "</ol>";
   
-	return $output;
+    return $output;
 }
 
 function getDataFromFeed( $url ) {
-  $data = simplexml_load_file( $url );
-	
-//	echo ($data) ? "<p>$url loaded data correctly</p>" : "<p>Bad data from $url</p>";
-//	print_r($data);
-	
-	return $data;
+    $data = simplexml_load_file( $url );
+    
+//    echo ($data) ? "<p>$url loaded data correctly</p>" : "<p>Bad data from $url</p>";
+//    print_r($data);
+    
+    return $data;
 }
 
 function getHtmlForEntry( $item, $stream='considered', $backlog = array() ) {
-  $item->pubDate = date('l jS F Y, H:i', strtotime($item->pubDate));
+    $item->pubDate = date('l jS F Y, H:i', strtotime($item->pubDate));
 
-  $html = '';
-  if ( sizeof( $backlog ) > 0 && !preg_match( '/^http:\/\/www\.flickr\.com/', $item->link ) ) {
+    $html = '';
+    if ( sizeof( $backlog ) > 0 && !preg_match( '/^http:\/\/www\.flickr\.com/', $item->link ) ) {
 
-    $html = getHtmlForEntryFlickrThumbnail($backlog, $stream);
-    $backlog = array();
-  }
-    
-  if ( preg_match( '/^http:\/\/www\.flickr\.com/', $item->link ) ) {
-    
-    if ( 'considered' == $stream ) {
-      return getHtmlForEntryFlickr($item);
-    } else {
-      $backlog[] = $item;
-      return '';
+        $html = getHtmlForEntryFlickrThumbnail($backlog, $stream);
+        $backlog = array();
     }
 
-  } else if ( preg_match( '/^del\.icio\.us link:\s*/', $item->title ) ) {
-    return $html.getHtmlForEntryDelicious($item);
-  } else if ( preg_match( '/^http:\/\/thetenwordreview\.com/', $item->link ) ) {
-    return $html.getHtmlForEntryTheTenWordReview($item);
-  } else if ( preg_match( '/^http:\/\/www\.last\.fm/', $item->link ) ) {
-    return $html.getHtmlForEntryLastFm($item);
-  } else if ( preg_match( '/^http:\/\/twitter\.com/', $item->link ) ) {
-    return $html.getHtmlForEntryTwitter($item);
-  } else if ( preg_match( '/^http:\/\/upcoming\.yahoo\.com/', $item->link ) ) {
-    return $html.getHtmlForEntryUpcoming($item);
-  }
-  
+    if ( preg_match( '/^http:\/\/www\.flickr\.com/', $item->link ) ) {
+
+        if ( 'considered' == $stream ) {
+            return getHtmlForEntryFlickr($item);
+        } else {
+            $backlog[] = $item;
+            return '';
+        }
+
+    } else if ( preg_match( '/^del\.icio\.us link:\s*/', $item->title ) ) {
+        return $html.getHtmlForEntryDelicious($item);
+    } else if ( preg_match( '/^http:\/\/thetenwordreview\.com/', $item->link ) ) {
+        return $html.getHtmlForEntryTheTenWordReview($item);
+    } else if ( preg_match( '/^http:\/\/www\.last\.fm/', $item->link ) ) {
+        return $html.getHtmlForEntryLastFm($item);
+    } else if ( preg_match( '/^http:\/\/twitter\.com/', $item->link ) ) {
+        return $html.getHtmlForEntryTwitter($item);
+    } else if ( preg_match( '/^http:\/\/upcoming\.yahoo\.com/', $item->link ) ) {
+        return $html.getHtmlForEntryUpcoming($item);
+    }
+
     $maxDescLen = 100;
     $description = strip_tags($item->description);
     if ( 'considered' != $stream && mb_strlen($description) > $maxDescLen ) {
         $description = mb_substr( $description, 0, $maxDescLen ).'&hellip;';
     }
   
-	return $html.<<<HTML
-		<li class='module'>
-			<div class='hd'>
-				<h3><a href='{$item->link}'>{$item->title}</a></h3>
-			</div>
-			<div class='bd'>
-				{$description}
-			</div>
-			<div class='ft'>
-				<p>$item->pubDate</p>
-			</div>
-		</li>
+    return $html.<<<HTML
+        <li class='module'>
+            <div class='hd'>
+                <h3><a href='{$item->link}'>{$item->title}</a></h3>
+            </div>
+            <div class='bd'>
+                {$description}
+            </div>
+            <div class='ft'>
+                <p>$item->pubDate</p>
+            </div>
+        </li>
 HTML;
 }
 
 function getHtmlForEntryUpcoming( $item ) {
-	$pos = strpos( $item->description, '<br />' );
-	if ( false !== $pos ) {
-	  $item->description = substr( $item->description, 0, $pos );
-	}
-	
-	$maxDescriptionLength = 70;
+    $pos = strpos( $item->description, '<br />' );
+    if ( false !== $pos ) {
+        $item->description = substr( $item->description, 0, $pos );
+    }
+    
+    $maxDescriptionLength = 70;
 
-	$description = $item->description;
-	if ( mb_strlen($description) > $maxDescriptionLength ) {
-		$description = mb_substr( $description, 0, $maxDescriptionLength) . '&hellip;';
-	}
-	
-	$pos = strpos( $item->title, ':' );
-	if ( false !== $pos ) {
-	  $date = substr( $item->title, 0, $pos );
-	  $item->title = substr( $item->title, $pos + 2 );
-	  
-	  preg_match( '/(\w+)\s+(\d+),\s+(\d+)/', $date, $matches );
-	  $month = $matches[1];
-	  $day   = $matches[2];
-	  $year  = $matches[3];
-	}
-	
-	$maxTitleLength = 23;
+    $description = $item->description;
+    if ( mb_strlen($description) > $maxDescriptionLength ) {
+        $description = mb_substr( $description, 0, $maxDescriptionLength) . '&hellip;';
+    }
+    
+    $pos = strpos( $item->title, ':' );
+    if ( false !== $pos ) {
+        $date = substr( $item->title, 0, $pos );
+        $item->title = substr( $item->title, $pos + 2 );
 
-	$title = $item->title;
-	if ( mb_strlen($title) > $maxTitleLength ) {
-		$title = mb_substr( $title, 0, $maxTitleLength) . '&hellip;';
-	}
-	
-	return <<<HTML
-		<li class='module upcoming'>
-			<div class='hd'>
-				<h3><a href='{$item->link}'>{$title}</a></h3>
-			</div>
-			<div class='bd'>
-				<p class='date'><span>{$month} {$year}</span>{$day}</p>
-				<p>{$description}</p>
-			</div>
-			<div class='ft'>
-				<p>{$item->pubDate}</p>
-			</div>
-		</li>
+        preg_match( '/(\w+)\s+(\d+),\s+(\d+)/', $date, $matches );
+        $month = $matches[1];
+        $day   = $matches[2];
+        $year  = $matches[3];
+    }
+    
+    $maxTitleLength = 23;
+
+    $title = $item->title;
+    if ( mb_strlen($title) > $maxTitleLength ) {
+        $title = mb_substr( $title, 0, $maxTitleLength) . '&hellip;';
+    }
+    
+    return <<<HTML
+        <li class='module upcoming'>
+            <div class='hd'>
+                <h3><a href='{$item->link}'>{$title}</a></h3>
+            </div>
+            <div class='bd'>
+                <p class='date'><span>{$month} {$year}</span>{$day}</p>
+                <p>{$description}</p>
+            </div>
+            <div class='ft'>
+                <p>{$item->pubDate}</p>
+            </div>
+        </li>
 HTML;
 }
 
 function getHtmlForEntryFlickr( $item ) {
-	$description = $item->description;
-	preg_match( '/(http:\/\/farm\d+\.static\.flickr\.com\/\d+\/[^.]*)_m.jpg" width="(\d+)" height="(\d+)/', $description, $matches);
-	
-	if ( $matches ) {
-		$width = $matches[2];
-		$height = $matches[3];
-		
-		$maxWidth = 469;
-		$maxHeight = 469;
-		
-		//if ( $width > $height) {
-			$height = ($maxWidth / $width) * $height;
-			$width  = $maxWidth;
-		//} else {
-		//	$width  = ($maxHeight / $height) * $width;
-		//	$height = $maxHeight;
-		//}
-		
-		$description = "<p><a href='{$item->link}'><img src='{$matches[1]}.jpg' width='{$width}' height='{$height}' alt='{$item->title}'></a></p>";
-	}
-	
-	return <<<HTML
-		<li class='module flickr'>
-			<div class='hd'>
-				<h3><a href='{$item->link}'>{$item->title}</a></h3>
-			</div>
-			<div class='bd'>
-				{$description}
-			</div>
-			<div class='ft'>
-				<p>{$item->pubDate}</p>
-			</div>
-		</li>
+    $description = $item->description;
+    preg_match( '/(http:\/\/farm\d+\.static\.flickr\.com\/\d+\/[^.]*)_m.jpg" width="(\d+)" height="(\d+)/', $description, $matches);
+    
+    if ( $matches ) {
+        $width = $matches[2];
+        $height = $matches[3];
+        
+        $maxWidth = 469;
+        $maxHeight = 469;
+        
+        //if ( $width > $height) {
+            $height = ($maxWidth / $width) * $height;
+            $width  = $maxWidth;
+        //} else {
+        //  $width  = ($maxHeight / $height) * $width;
+        //  $height = $maxHeight;
+        //}
+        
+        $description = "<p><a href='{$item->link}'><img src='{$matches[1]}.jpg' width='{$width}' height='{$height}' alt='{$item->title}'></a></p>";
+    }
+    
+    return <<<HTML
+        <li class='module flickr'>
+            <div class='hd'>
+                <h3><a href='{$item->link}'>{$item->title}</a></h3>
+            </div>
+            <div class='bd'>
+                {$description}
+            </div>
+            <div class='ft'>
+                <p>{$item->pubDate}</p>
+            </div>
+        </li>
 HTML;
 }
 
 function getHtmlForEntryFlickrThumbnail( $items, $stream ) {
-	$output = '';
-	foreach ( $items as $item ) {
-		$title = $item->title;
-		if ( 'consciousness' == $stream ) {
-			$title = 'flickr favourite: '.$title;
-		}
+    $output = '';
+    foreach ( $items as $item ) {
+        $title = $item->title;
+        if ( 'consciousness' == $stream ) {
+            $title = 'flickr favourite: '.$title;
+        }
 
-		$description = $item->description;
-		//$item->description = preg_replace( '/(http:\/\/farm\d+\.static\.flickr\.com\/\d+\/[^.]*)_m.jpg" width="\d+" height="\d+/', '$1_t.jpg', $item->description);
-		if (preg_match( '/(http:\/\/farm\d+\.static\.flickr\.com\/\d+\/[^.]*)_m.jpg/', $item->description, $matches) ) {
-			$description = "<a href='{$item->link}'><img src='{$matches[1]}_s.jpg' alt='$title'></a>";
-		}
+        $description = $item->description;
+        //$item->description = preg_replace( '/(http:\/\/farm\d+\.static\.flickr\.com\/\d+\/[^.]*)_m.jpg" width="\d+" height="\d+/', '$1_t.jpg', $item->description);
+        if (preg_match( '/(http:\/\/farm\d+\.static\.flickr\.com\/\d+\/[^.]*)_m.jpg/', $item->description, $matches) ) {
+            $description = "<a href='{$item->link}'><img src='{$matches[1]}_s.jpg' alt='$title'></a>";
+        }
 
 
-		$output .= "<li>{$description}</li>";
-	}
+        $output .= "<li>{$description}</li>";
+    }
 
-	return <<<HTML
-		<li class='module flickr'>
-			<div class='hd'>
-				<h3>Flickr</h3>
-			</div>
-			<div class='bd'>
-				<ol>$output</ol>
-			</div>
-			<div class='ft'>
-				<p>{$item->pubDate}</p>
-			</div>
-		</li>
+    return <<<HTML
+        <li class='module flickr'>
+            <div class='hd'>
+                <h3>Flickr</h3>
+            </div>
+            <div class='bd'>
+                <ol>$output</ol>
+            </div>
+            <div class='ft'>
+                <p>{$item->pubDate}</p>
+            </div>
+        </li>
 HTML;
-	}
+    }
 
 function getHtmlForEntryTheTenWordReview( $item ) {
-	
-	$tenWordColors = array( 
-		'000000',
-		'8c288b', 
-		'f6821f',
-		'87c80a',
-		'00adef',
-		'000000',
-		'8c288b', 
-		'f6821f',
-		'87c80a',
-		'00adef',
-	);
-	
-	$description = (string) $item->description;
-	$description = simplexml_load_string( "<?xml version='1.0'?><description>$description</description>" );
+    
+    $tenWordColors = array( 
+        '000000',
+        '8c288b', 
+        'f6821f',
+        '87c80a',
+        '00adef',
+        '000000',
+        '8c288b', 
+        'f6821f',
+        '87c80a',
+        '00adef',
+    );
+    
+    $description = (string) $item->description;
+    $description = simplexml_load_string( "<?xml version='1.0'?><description>$description</description>" );
 
-	$tenWordCount = substr( number_format( hexdec( md5( $description->p[0] ) ) ), -1);
-	
-	preg_match('/^(.*) by /', $item->title, $matches);
-	$title = ($matches) ? $matches[1] : $item->title;
+    $tenWordCount = substr( number_format( hexdec( md5( $description->p[0] ) ) ), -1);
+    
+    preg_match('/^(.*) by /', $item->title, $matches);
+    $title = ($matches) ? $matches[1] : $item->title;
 
-	return <<<HTML
-		<li class='module thetenwordreview' style='background-color: #{$tenWordColors[$tenWordCount]}'>
-			<div class='hd'>
-				<h3><a href='{$item->link}'>$title</a></h3>
-			</div>
-			<div class='bd'>
-				{$description->p[0]}
-				<p class='icon'><a href='http://thetenwordreview.com/users/workingwithme'><img src='http://thetenwordreview.com/images/icons/workingwithme.png' alt='workingwithme&apos;s icon'></a></p>
-			</div>
-			<div class='ft'>
-				<p>{$item->pubDate}</p>
-			</div>
-		</li>
+    return <<<HTML
+        <li class='module thetenwordreview' style='background-color: #{$tenWordColors[$tenWordCount]}'>
+            <div class='hd'>
+                <h3><a href='{$item->link}'>$title</a></h3>
+            </div>
+            <div class='bd'>
+                {$description->p[0]}
+                <p class='icon'><a href='http://thetenwordreview.com/users/workingwithme'><img src='http://thetenwordreview.com/images/icons/workingwithme.png' alt='workingwithme&apos;s icon'></a></p>
+            </div>
+            <div class='ft'>
+                <p>{$item->pubDate}</p>
+            </div>
+        </li>
 HTML;
 }
 
 function getHtmlForEntryLastFm( $item ) {
-	return <<<HTML
-		<li class='module lastfm'>
-			<div class='hd'>
-				<h3><span>Last.fm: </span><a href='{$item->link}'>{$item->title}</a></h3>
-			</div>
-			<div class='bd'>
-				<p>{$item->pubDate}</p>
-			</div>
-			<div class='ft'>
-			</div>
-		</li>
+    return <<<HTML
+        <li class='module lastfm'>
+            <div class='hd'>
+                <h3><span>Last.fm: </span><a href='{$item->link}'>{$item->title}</a></h3>
+            </div>
+            <div class='bd'>
+                <p>{$item->pubDate}</p>
+            </div>
+            <div class='ft'>
+            </div>
+        </li>
 HTML;
 }
 
 function getHtmlForEntryDelicious( $item ) {
-	$maxDescriptionLength = 70;
+    $maxDescriptionLength = 70;
 
-	$description = $item->description;
-	if ( mb_strlen($description) > $maxDescriptionLength ) {
-		$description = mb_substr( $description, 0, $maxDescriptionLength) . '&hellip;';
-	}
-	
-	preg_match( '/^del\.icio\.us link:\s*(.*)/', $item->title, $matches);
+    $description = $item->description;
+    if ( mb_strlen($description) > $maxDescriptionLength ) {
+        $description = mb_substr( $description, 0, $maxDescriptionLength) . '&hellip;';
+    }
+    
+    preg_match( '/^del\.icio\.us link:\s*(.*)/', $item->title, $matches);
 
-	return <<<HTML
-		<li class='module delicious'>
-			<div class='hd'>
-				<h3><a href='{$item->link}'>{$matches[1]}</a></h3>
-			</div>
-			<div class='bd'>
-				<p>$description</p>
-			</div>
-			<div class='ft'>
-				<p>{$item->pubDate}</p>
-			</div>
-		</li>
+    return <<<HTML
+        <li class='module delicious'>
+            <div class='hd'>
+                <h3><a href='{$item->link}'>{$matches[1]}</a></h3>
+            </div>
+            <div class='bd'>
+                <p>$description</p>
+            </div>
+            <div class='ft'>
+                <p>{$item->pubDate}</p>
+            </div>
+        </li>
 HTML;
 }
 
 function getHtmlForEntryTwitter( $item ) {
-	$twitterUsername = 'NeilCrosby';
-	
-	$description = strip_tags($item->description);
-	$class = '';
-	if ( $twitterUsername != mb_substr($item->author[0], 0, strlen($twitterUsername)) && $twitterUsername != mb_substr($description, 0, strlen($twitterUsername)) ) {
-		$author = $item->author[0];
-		preg_match('/(^[^\s]+)/', $author, $matches);
-		$description = $matches[1] . ': ' . $description;
-		$class=' twitter_at_user';
- 	}
-	
-	return <<<HTML
-		<li class='module twitter$class'>
-			<div class='hd'>
-				<h3><a href='{$item->link}'>Twitter</a></h3>
-			</div>
-			<div class='bd'>
-				<p><a href='{$item->link}'>{$description}</a></p>
-			</div>
-			<div class='ft'>
-				<p>{$item->pubDate}</p>
-			</div>
-		</li>
+    $twitterUsername = 'NeilCrosby';
+    
+    $description = strip_tags($item->description);
+    $class = '';
+    if ( $twitterUsername != mb_substr($item->author[0], 0, strlen($twitterUsername)) && $twitterUsername != mb_substr($description, 0, strlen($twitterUsername)) ) {
+        $author = $item->author[0];
+        preg_match('/(^[^\s]+)/', $author, $matches);
+        $description = $matches[1] . ': ' . $description;
+        $class=' twitter_at_user';
+    }
+    
+    return <<<HTML
+        <li class='module twitter$class'>
+            <div class='hd'>
+                <h3><a href='{$item->link}'>Twitter</a></h3>
+            </div>
+            <div class='bd'>
+                <p><a href='{$item->link}'>{$description}</a></p>
+            </div>
+            <div class='ft'>
+                <p>{$item->pubDate}</p>
+            </div>
+        </li>
 HTML;
 }
 
