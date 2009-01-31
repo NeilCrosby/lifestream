f4d692c3de9f1601ebdba2e94729599f96e80537
Neil Crosby <neil@thetenwordreview.com>
2009-01-31 23:12:41 +0000

    Now works nicely with 'MyBrand'ed Feedburnered feeds

--- feeds.php
+++ feeds.php
@@ -1,25 +1,27 @@
 <?php
 
 $options = array(
-    'considered' => 'http://pipes.yahoo.com/pipes/pipe.run?_id=ADbNqOil3BGGzfPa6kjTQA&_render=rss',
-//  'considered' => 'http://feeds.feedburner.com/NeilCrosbyLifestreamConsidered?format=xml',
-    'stream-of-consciousness' => 'http://pipes.yahoo.com/pipes/pipe.run?_id=f41d64550e674b7f01bad0f3c49d46f8&_render=rss',
-    'others-said' => 'http://pipes.yahoo.com/pipes/pipe.run?_id=ygKh4Siu3BGqyNyQJphxuA&_render=rss',
+    'considered' => 'http://feeds.neilcrosby.com/NeilCrosbyLifestreamConsidered',
+    'stream-of-consciousness' => 'http://feeds.neilcrosby.com/NeilCrosbyLifestreamOfConsciousness',
+    'others-said' => 'http://feeds.neilcrosby.com/NeilCrosbyOthersSaid',
 );
 
 $optionsFeedburner = array(
     'considered' => 'http://pipes.yahoo.com/pipes/pipe.run?_id=ADbNqOil3BGGzfPa6kjTQA&_render=rss',
+    'stream-of-consciousness' => 'http://pipes.yahoo.com/pipes/pipe.run?_id=f41d64550e674b7f01bad0f3c49d46f8&_render=rss',
+    'others-said' => 'http://pipes.yahoo.com/pipes/pipe.run?_id=ygKh4Siu3BGqyNyQJphxuA&_render=rss',
 );
 
 if ( !isset( $_GET['feed'] ) || !array_key_exists( $_GET['feed'], $options ) ) {
     exit;
 }
 
-header("Content-Type: application/rss+xml");
 if ('FeedBurner' == mb_substr($_SERVER['USER_AGENT'], 0, mb_strlen('FeedBurner'))) {
+    header("Content-Type: application/rss+xml");
     readfile( $optionsFeedburner[$_GET['feed']] );
 } else {
-    readfile( $options[$_GET['feed']] );
+    //readfile( $options[$_GET['feed']] );
+    header('Location: '.$options[$_GET['feed']]);
 }
 
 ?>
